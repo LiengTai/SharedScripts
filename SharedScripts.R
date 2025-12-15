0c3087fbcbf5d3a05541d38b5d303441151abcfa
Lieng <lieng.taing@gmail.com>
2025-12-15 18:17:20 +0100

    Test Treatment on Cluster

--- SharedScripts.R
+++ SharedScripts.R
@@ -186,3 +186,108 @@ GenerateLocalResultsAllGOs<-function(
   return(out)
 }
 
+
+
+#' Title
+#'
+#' @param ChunkName 
+#' @param ResultsDF 
+#' @param SeuratObject 
+#' @param OrgDb 
+#' @param Levels 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+GenerateLocalTreatmentResultsAllGOs<-function(
+    ChunkName="Test",
+    ResultsDF="merged_seurat.markers[merged_seurat.markers$cluster=='2',]",
+    Universe="rownames(merged_seurat)",
+    SeuratObject="merged_seurat",
+    Identity="seurat_clusters",
+    OrgDb="org.Mm.eg.db",
+    LocalClusterName="0",
+    LocalContrastName="orig.ident",
+    Levels=3){
+  if(Universe=="rownames(merged_seurat)" & SeuratObject!="merged_seurat") {Universe=paste0("rownames(",SeuratObject,")")}
+  out<-c()
+  out<-c(out,paste0(paste0(rep(":",Levels+1),collapse = "")," {.panel-tabset}\n\n"))  
+  out<-c(out,paste0(paste0(rep("#",Levels),collapse = "")," Differentially expressed\n\n"))
+  out<-c(out,paste0("```{r ",ChunkName," DEG}\n"))
+  out<-c(out,"if(!'package:clusterProfiler' %in% search()){library(clusterProfiler)}\n")
+  out<-c(out,"if(!'package:enrichplot' %in% search()){library(enrichplot)}\n")
+  out<-c(out,"if(!'package:utils' %in% search()){library(utils)}\n")
+  out<-c(out,paste0("LocalResults<-",ResultsDF,"\n"))
+  out<-c(out,"if(sum(LocalResults$p_val_adj<0.05)>1){LocalResults<-LocalResults[LocalResults$p_val_adj<0.05, ]}\n")
+  out<-c(out,"LocalResults<-LocalResults[order(LocalResults$avg_log2FC,decreasing=TRUE), ]\n")
+  out<-c(out,"LocalMarker<-LocalResults\n")
+  out<-c(out,"LocalMarker<-LocalMarker[order(LocalResults$avg_log2FC,decreasing=TRUE),]\n")
+  out<-c(out,"if(sum(LocalMarker$pct.1>0.5)>1){LocalMarker<-LocalMarker[LocalMarker$pct.1>0.5,]}\n")
+  out<-c(out,"LocalMarker<-rownames(LocalMarker)[1]\n")
+  out<-c(out,"LocalMarker<-gsub('\\\\.[0-9]*$','',LocalMarker)\n")
+  out<-c(out,paste0("LocalDiffData<-",SeuratObject,"[,",SeuratObject,"@meta.data$",seurat_clusters,"=='",LocalClusterName,"']"))
+  out<-c(out,paste0("LocalDiffData<-SetIdent(LocalDiffData,value = '",LocalContrastName,"'"))
+  out<-c(out,paste0("DotPlot(LocalDiffData,features = c(LocalResults$Feature[1:5],LocalResults$Feature[(length(LocalResults$Feature)-5):length(LocalResults$Feature)]))+RotatedAxis()"))
+  out<-c(out,"LocalResults[,'avg_log2FC']<-formatC(LocalResults[,'avg_log2FC'], format = 'e', digits = 2)\n")
+  out<-c(out,"LocalResults[,'p_val']<-formatC(LocalResults[,'p_val'], format = 'e', digits = 2)\n")
+  out<-c(out,"LocalResults[,'p_val_adj']<-formatC(LocalResults[,'p_val_adj'], format = 'e', digits = 2)\n")
+  out<-c(out,"DT::datatable(LocalResults[,c('gene','pct.1','pct.2','avg_log2FC','p_val','p_val_adj')],colnames = c('Gene','pct in cluster','pct out of cluster','avg log2FC','p val','p val adj'),rownames = FALSE, extensions = 'Buttons', options = list( dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')),escape=FALSE)\n")
+  out<-c(out,"```\n")
+  out<-c(out,GenerateAllGOsCode(
+    ChunkName=paste(ChunkName,"DEG"),
+    Universe=Universe,
+    OrgDb=OrgDb,
+    Levels=Levels+1))
+  
+  
+  out<-c(out,paste0(paste0(rep("#",Levels),collapse = "")," UpRegulated\n\n"))
+  out<-c(out,paste0("```{r ",ChunkName," UP}\n"))
+  out<-c(out,paste0("LocalResults<-",ResultsDF,"\n"))
+  out<-c(out,"LocalResults<-LocalResults[LocalResults$avg_log2FC>0, ]\n")
+  out<-c(out,"if(sum(LocalResults$p_val_adj<0.05)>1){LocalResults<-LocalResults[LocalResults$p_val_adj<0.05, ]}\n")
+  out<-c(out,"LocalResults<-LocalResults[order(LocalResults$avg_log2FC,decreasing=TRUE), ]\n")
+  out<-c(out,"LocalMarker<-LocalResults\n")
+  out<-c(out,"if(sum(LocalMarker$pct.1>0.5)>1){LocalMarker<-LocalMarker[LocalMarker$pct.1>0.5,]}\n")
+  out<-c(out,paste0("LocalDiffData<-",SeuratObject,"[,",SeuratObject,"@meta.data$",seurat_clusters,"=='",LocalClusterName,"']"))
+  out<-c(out,paste0("LocalDiffData<-SetIdent(LocalDiffData,value = '",LocalContrastName,"'"))
+  out<-c(out,paste0("DotPlot(LocalDiffData,features = c(LocalResults$Feature[1:5],LocalResults$Feature[(length(LocalResults$Feature)-5):length(LocalResults$Feature)]))+RotatedAxis()"))
+  out<-c(out,"LocalResults[,'avg_log2FC']<-formatC(LocalResults[,'avg_log2FC'], format = 'e', digits = 2)\n")
+  out<-c(out,"LocalResults[,'p_val']<-formatC(LocalResults[,'p_val'], format = 'e', digits = 2)\n")
+  out<-c(out,"LocalResults[,'p_val_adj']<-formatC(LocalResults[,'p_val_adj'], format = 'e', digits = 2)\n")
+  out<-c(out,"DT::datatable(LocalResults[,c('gene','pct.1','pct.2','avg_log2FC','p_val','p_val_adj')],colnames = c('Gene','pct in cluster','pct out of cluster','avg log2FC','p val','p val adj'),rownames = FALSE, extensions = 'Buttons', options = list( dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')),escape=FALSE)\n")
+  out<-c(out,"```\n")
+  out<-c(out,GenerateAllGOsCode(
+    ChunkName=paste(ChunkName,"UP"),
+    Universe=Universe,
+    OrgDb=OrgDb,
+    Levels=Levels+1))
+  
+  out<-c(out,paste0(paste0(rep("#",Levels),collapse = "")," DownRegulated\n\n"))
+  out<-c(out,paste0("```{r ",ChunkName," DOWN}\n"))
+  out<-c(out,paste0("LocalResults<-",ResultsDF,"\n"))
+  out<-c(out,"if(!'gene' %in% colnames(LocalResults)){LocalResults$gene<-rownames(LocalResults)}\n")
+  out<-c(out,"if(sum(LocalResults$avg_log2FC<0)>1){LocalResults<-LocalResults[LocalResults$avg_log2FC<0, ]}\n")
+  out<-c(out,"if(sum(LocalResults$p_val_adj<0.05)>1){LocalResults<-LocalResults[LocalResults$p_val_adj<0.05, ]}\n")
+  out<-c(out,"LocalResults<-LocalResults[order(LocalResults$avg_log2FC,decreasing=FALSE), ]\n")
+  out<-c(out,"LocalMarker<-LocalResults\n")
+  out<-c(out,"if(sum(LocalMarker$pct.2>0.5)>1){LocalMarker<-LocalMarker[LocalMarker$pct.2>0.5,]}\n")
+  out<-c(out,"LocalMarker<-rownames(LocalMarker)[1]\n")
+  out<-c(out,paste0("LocalDiffData<-",SeuratObject,"[,",SeuratObject,"@meta.data$",seurat_clusters,"=='",LocalClusterName,"']"))
+  out<-c(out,paste0("LocalDiffData<-SetIdent(LocalDiffData,value = '",LocalContrastName,"'"))
+  out<-c(out,paste0("DotPlot(LocalDiffData,features = c(LocalResults$Feature[1:5],LocalResults$Feature[(length(LocalResults$Feature)-5):length(LocalResults$Feature)]))+RotatedAxis()"))
+  out<-c(out,"LocalResults[,'avg_log2FC']<-formatC(LocalResults[,'avg_log2FC'], format = 'e', digits = 2)\n")
+  out<-c(out,"LocalResults[,'p_val']<-formatC(LocalResults[,'p_val'], format = 'e', digits = 2)\n")
+  out<-c(out,"LocalResults[,'p_val_adj']<-formatC(LocalResults[,'p_val_adj'], format = 'e', digits = 2)\n")
+  out<-c(out,"DT::datatable(LocalResults[,c('gene','pct.1','pct.2','avg_log2FC','p_val','p_val_adj')],colnames = c('Gene','pct in cluster','pct out of cluster','avg log2FC','p val','p val adj'),rownames = FALSE, extensions = 'Buttons', options = list( dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')),escape=FALSE)\n")
+  out<-c(out,"```\n")
+  out<-c(out,GenerateAllGOsCode(
+    ChunkName=paste(ChunkName,"DOWN"),
+    Universe=Universe,
+    OrgDb=OrgDb,
+    Levels=Levels+1))
+  out<-c(out,paste0(paste0(rep(":",Levels+1),collapse = ""),"\n\n"))  
+  out<-paste(out,collapse = "",sep="")
+  return(out)
+}
+
